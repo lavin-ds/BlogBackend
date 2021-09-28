bc91b20dadc3686f102b0f5827b7585c26d47dca
lds <lavin.github@outlook.com>
2021-09-29 00:25:58 +0200

    Add blogcontroller

---
+++ Api.Proj/Controllers/BlogController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Proj.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BlogController : ControllerBase
+    {
+        private readonly ILogger<BlogController> _logger;
+
+        public BlogController(ILogger<BlogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<BlogPost> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public BlogPost Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        [HttpPost]
+        public void Post([FromBody] BlogPost blogPost)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] BlogPost blogPost)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {   
+            throw new NotImplementedException();
+        }
+    }
+}
